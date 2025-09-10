5148f0e20760182fa8ead9c7be79dc88bb2bd367
Valay Bhaladhare <178009633+Valay-2004@users.noreply.github.com>
2025-09-10 11:05:23 +0000

    "Added MoveNegatives.java"

---
+++ Arrays/MoveNegatives.java
@@ -0,0 +1,16 @@
+
+// Moving all the negatives to the front side of the array
+public class MoveNegatives {
+    public static void main(String[] args){
+
+    }
+
+    // Using Quick Sort Partition   |   Time : O(n) , Space : O(1)
+    public static quickSortMoveNeg(int[] arr){
+        int n = arr.length;
+        int j = 0;
+        for(int i = 0; i < n; i++){
+
+        }
+    }
+}
